17cd5246bf8738a7557a968cc7ea338b70d72cf2
chetandeep15 <chetan.deep24@gmail.com>
2016-10-12 16:43:19 +0530

    improves specs and changes homepage

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,55 +1,50 @@
 require 'rails_helper'
 
 describe "Static pages" do
+  subject { page }
 
   describe "Home page" do
+    before { visit root_path}
 
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_selector('h1', text: "Sample App")
+      expect(subject).to have_selector('h1', text: "Sample App")
     end
 
     it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Sample App | Home")
+      expect(subject).to have_title("Sample App | Home")
     end
   end
 
   describe "Help page" do
-
+    before { visit help_path }
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_selector('h1', text: "Help")
+      expect(subject).to have_selector('h1', text: "Help")
     end
 
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_title("Sample App | Help")
+      expect(subject).to have_title("Sample App | Help")
     end
   end
 
   describe "About page" do
-
+    before { visit about_path }
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_selector('h1', text: "About Us")
+      expect(subject).to have_selector('h1', text: "About Us")
     end
 
     it "should have the title 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_title ("Sample App | About Us")
+      expect(subject).to have_title ("Sample App | About Us")
     end
   end
 
   describe "Contact page" do
+    before { visit contact_path }
     it "should have the content 'Contact Us'" do
-      visit 'static_pages/contact'
-      expect(page).to have_content('Contact Us')
+      expect(subject).to have_content('Contact Us')
     end
 
     it "should have title 'Contact Us'" do
-      visit '/static_pages/contact'
-      expect(page).to have_title("Sample App | Contact Us")
+      expect(subject).to have_title("Sample App | Contact Us")
     end
 
   end
