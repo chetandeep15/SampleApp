69065448a8d7374e85ea4df5b5a88305e5073c66
chetandeep15 <chetan.deep24@gmail.com>
2016-10-14 00:35:42 +0530

    adds signin feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
-  attr_accessor :password, :password_confirmation
+  has_secure_password
   before_save{ |user| user.email = email.downcase }
+  before_save :create_remember_token
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates_presence_of :name
@@ -16,4 +17,9 @@ class User < ActiveRecord::Base
   validates_length_of :password, minimum: 6
 
   validates_presence_of :password_confirmation
+
+  private
+  def create_remember_token
+    self.remember_token = SecureRandom.urlsafe_base64
+  end
 end
