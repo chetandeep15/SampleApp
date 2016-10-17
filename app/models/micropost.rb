24e898a0fe64e25519765bf6662011e27e0b9b9b
chetandeep15 <chetan.deep24@gmail.com>
2016-10-17 17:15:25 +0530

    adds specs

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,9 +1,11 @@
 class Micropost < ActiveRecord::Base
-  attr_accessible :content, :user_id
+  attr_accessible :content
   belongs_to :user
 
   validates_presence_of :user_id
   validates_presence_of :content
+
+  validates_length_of :content, maximum: 140
 
   default_scope order: 'microposts.created_at DESC'
 
--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -8,31 +8,23 @@ describe Relationship do
 
   subject { relationship }
 
-  it { should be_valid }
-
-  describe "accessible attributes" do
-    it "should not allow access to follower_id" do
-      expect do
-        Relationship.new(follower_id: follower.id)
-      end.should raise_error(ActiveModel::MassAssignmentSecurity::Error)
-    end
-  end
+  it { is_expected.to be_valid }
 
   describe "follower methods" do
 
-    it { should respond_to(:follower) }
-    it { should respond_to(:followed) }
-    its(:follower) { should == follower }
-    its(:followed) { should == followed }
+    it { is_expected.to respond_to(:follower) }
+    it { is_expected.to respond_to(:followed) }
+    its(:follower) { is_expected.to eq(follower) }
+    its(:followed) { is_expected.to eq(followed) }
   end
 
   describe "when follower id is not present" do
     before { relationship.follower_id = nil }
-    it { should_not be_valid }
+    it { is_expected.to_not be_valid }
   end
 
   describe "when followed id is not present" do
     before { relationship.followed_id = nil }
-    it { should_not be_valid }
+    it { is_expected.to_not be_valid }
   end
 end
